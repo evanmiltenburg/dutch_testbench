a85b8b91f6a6cd95fa32e83d22bfdf2b3a483d02
Emiel <emielonline@gmail.com>
2015-09-15 22:40:08 +0200

    Use xlrd instead of .txt to read in data more reliably

--- dutch_testbench/dedeyne_storms_relatedness.py
+++ dutch_testbench/dedeyne_storms_relatedness.py
@@ -1,6 +1,7 @@
+import xlrd
 from collections import namedtuple
 
-path = 'dutch_testbench/DeDeyne-BRM-2008/DeDeyne(2008).txt'
+path = 'dutch_testbench/DeDeyne-BRM-2008/DeDeyne(2008).xls'
 Associations = namedtuple('Associations',['a1','f1','a2','f2','a3','f3'])
 
 def get_association_dict():
@@ -8,13 +9,13 @@ def get_association_dict():
     
     def create_entry(line):
         "Create an entry for the dictionary, on the basis of a line from the file."
-        row = line.strip().split(';')
+        row = [cell.value for cell in line]
         word = row[0]
-        data = map(lambda x: int(x) if x.isdigit() else x, row[2:8])
-        return (word,Associations(*data))
-
-    with open(path) as f:
-        f.readline()
-        d = dict(create_entry(line) for line in f)
+        return (word,Associations(*row[2:8]))
+    
+    book = xlrd.open_workbook(path)
+    sheet = book.sheet_by_name('associations')
+    
+    d = dict(create_entry(sheet.row(rownum)) for rownum in range(1,sheet.nrows))
     
     return d
